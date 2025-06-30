376c6983dfb0b6fe687f80b63838a88a21acfc45
Wuor Bhang <143351848+WuorBhang@users.noreply.github.com>
2025-06-30 23:37:44 +0300

    Create 0-stream_users.py

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,15 @@
+import mysql.connector
+from seed import connect_to_prodev
+
+def stream_users():
+    connection = connect_to_prodev()
+    if connection:
+        try:
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM user_data")
+            for row in cursor:
+                yield row
+            cursor.close()
+            connection.close()
+        except mysql.connector.Error as err:
+            print(f"Error streaming data: {err}")
