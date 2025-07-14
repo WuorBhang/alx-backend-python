4bed2ed19cfa66c6965d7d27be0bf8e7a8b17e17
WuorBhang <uhuribhang211@gmail.com>
2025-07-14 21:16:09 +0300

    add the messaging users app of new changes

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 from .views import UserViewSet, ConversationViewSet, MessageViewSet
 
-router = routers.DefaultRouter()
+# Root router
+router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+
+# Nested router: messages under conversations
+conversations_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(conversations_router.urls)),
 ]
