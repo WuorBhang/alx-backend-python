89f6552b66a318c6b3077d7acb9a0e4c8a946081
WuorBhang <uhuribhang211@gmail.com>
2025-07-14 21:11:31 +0300

    add the messaging users app of new changes

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from . import views
+from rest_framework import routers
+from .views import UserViewSet, ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'conversations', views.ConversationViewSet, basename='conversation')
-router.register(r'messages', views.MessageViewSet, basename='message')
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
     path('', include(router.urls)),
