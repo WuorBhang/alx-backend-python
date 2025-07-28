a60b6633cf00f9a382187d3fcf4a4bf56c94c1f7
WuorBhang <uhuribhang211@gmail.com>
2025-07-28 16:27:03 +0300

    Fix merge conflicts and clean up tracked cache files

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -128,7 +128,6 @@ CHANNEL_LAYERS = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ],
--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -53,7 +53,7 @@ class OffensiveLanguageMiddleware:
         
         return self.get_response(request)
 
-class RolepermissionMiddleware:
+class RolePermissionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
